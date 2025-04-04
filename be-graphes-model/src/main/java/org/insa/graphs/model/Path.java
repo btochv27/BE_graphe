61d542772b54d3405e4d8720373f828202a75523
Chauve Bastien <chauv@insa-12630>
2025-04-04 08:53:36 +0200

    Implementation de getLength dans Path

--- be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
@@ -195,11 +195,13 @@ public class Path {
      * Compute the length of this path (in meters).
      *
      * @return Total length of the path (in meters).
-     * @deprecated Need to be implemented.
      */
     public float getLength() {
-        // TODO:
-        return 0;
+        float length = 0;
+        for(Arc a : arcs){
+            length += a.getLength();
+        }
+        return length;
     }
 
     /**
