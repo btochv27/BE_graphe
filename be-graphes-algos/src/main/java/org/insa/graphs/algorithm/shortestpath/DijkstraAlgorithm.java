a9a51718cec1b92b60ceabc8c5a12066aec8faed
JeaNeige <150901493+JeaNeige@users.noreply.github.com>
2025-04-11 10:26:14 +0200

    Label + début dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,11 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.lang.reflect.Array;
+import java.util.function.IntBinaryOperator;
+
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Label;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -17,6 +23,33 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         ShortestPathSolution solution = null;
 
         // TODO: implement the Dijkstra algorithm
+        Label [] tabLabels= new Label[data.getGraph().size()];
+        //Iter 0
+        int index=0;
+        for(Node N: data.getGraph().getNodes()){
+            tabLabels[index]=new Label(N,false,Integer.MAX_VALUE, null);
+            index++;
+        }
+        tabLabels[0].setCoutRealise(0);
+
+        for (index=0; index<data.getGraph().size(); index++){
+            //Chercher min
+            int min=Integer.MAX_VALUE;
+            int labelMin;
+            for (int j=0; j<data.getGraph().size(); j++){
+                if (tabLabels[j].getCoutRealise()<min && !(tabLabels[j].getMarque())){
+                    labelMin=j;
+                    min=tabLabels[j].getCoutRealise();
+                }
+            }
+            //Mettre a jour le tableau en iterrant sur les arc
+            for (){
+                tabLabels[h].getCoutRealise()=min(tabLabels[j].getCoutRealise(),tabLabels[labelMin].getCoutRealise()+);
+            }
+        }
+    
+
+
 
         // when the algorithm terminates, return the solution that has been found
         return solution;
---
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
@@ -0,0 +1,27 @@
+package org.insa.graphs.model;
+
+public class Label {
+    Node sommetCourant;
+    boolean marque;
+    int coutRealise;
+    Arc pere;
+
+
+    public Label(Node sommetCourant, boolean marque, int coutRealise, Arc pere){
+        this.sommetCourant=sommetCourant;
+        this.marque=marque;
+        this.coutRealise=coutRealise;
+        this.pere=pere;
+    }
+
+//Getters
+    public Node getSommetCourant(){return this.sommetCourant;}
+    public boolean getMarque(){return this.marque;}
+    public int getCoutRealise(){return this.coutRealise;}
+    public Arc getPere(){return this.pere;}
+
+//Setters
+    public void setMarque(boolean marque){this.marque=marque;}
+    public void setCoutRealise(int coutRealise){this.coutRealise=coutRealise;}
+    public void setPere(Arc pere){this.pere=pere;}
+}
